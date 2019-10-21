c817269ee80f0ca8dcfd7a0df2d6d5227bc78705
Brandon <brandonsamuel72@gmail.com>
2019-10-21 15:23:41 -0700

    added rank and image link components and added styles to page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Nav/Navbar';
 import Logo from './components/Logo/Logo';
+import ImageLink from './components/ImageLink/ImageLink';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+
+const particleOptions = {
+  particles: {
+    "number": {
+      "value": 120,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={particleOptions} />
         <Navbar />
         <Logo />
+        <Rank />
+        <ImageLink />
       </div>
     );
   }
