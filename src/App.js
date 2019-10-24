3daa723f6e0e7622e7364fe48479a787963d7a5f
Brandon <brandonsamuel72@gmail.com>
2019-10-24 16:16:38 -0700

    added face recognition component, linked with image link component, installed clarifai pkg for face recognition

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Navbar from './components/Nav/Navbar';
 import Logo from './components/Logo/Logo';
 import ImageLink from './components/ImageLink/ImageLink';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
+require('dotenv').config();
+
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_API_KEY
+ });
 
 const particleOptions = {
   particles: {
@@ -21,6 +28,33 @@ const particleOptions = {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+onInputChange = (e) => {
+  this.setState({ input: e.target.value });
+}
+
+
+onButtonSubmit = () => {
+  this.setState({ imageUrl: this.state.input })
+  app.models
+  .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+  .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+}
+
   render() {
     return (
       <div className="App">
@@ -28,7 +62,8 @@ class App extends Component {
         <Navbar />
         <Logo />
         <Rank />
-        <ImageLink />
+        <ImageLink onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
