7c3222f54e0444018b704eb917dd5fddbbc90986
Brandon <brandonsamuel72@gmail.com>
2019-10-30 16:11:46 -0700

    update db

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    fetch('http://localhost:8080/imageurl', {
+    fetch('https://shielded-dusk-54451.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if(response) {
-        fetch('http://localhost:8080/image', {
+        fetch('https://shielded-dusk-54451.herokuapp.com:8080/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
