bf8f2e880ee28ce8e4f82f8c45da70282bc4421f
Brandon <brandonsamuel72@gmail.com>
2019-10-30 00:28:41 -0700

    register frontend working with backend

--- src/App.js
+++ src/App.js
@@ -38,7 +38,24 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joineds
+    }})
   }
 
   onInputChange = (e) => {
@@ -95,7 +112,7 @@ class App extends Component {
           : (
             this.state.route === 'signin'
             ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           ) 
         }
       </div>
